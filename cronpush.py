8aaebe9dc34da6f3c0ef0f9d1c8d638db4ae8bd9
zhang <zzh1989@126.com>
2014-01-21 13:39:54 +0800

    Update cronpush.py
    fix bug

--- cronpush.py
+++ cronpush.py
@@ -18,28 +18,26 @@ import model
 from config import *
 
 
-if __name__ == "__main__":
-	tz = pytz.timezone('GMT')
-	date = datetime.now(tz)
-	hour = date.hour
-	users =  model.get_current_push_users(hour)
-	if len(users) == 0:
-		print '-=end=-'
-	else:
-		#推送
-		for user in users:
-			feeds = []
-			ownfeeds = model.username2feeds(user.name)
-			if len(ownfeeds) != 0:
-				books = (model.get_allbooks())
-				for book in books:
-					if book.f_id in ownfeeds:
-						b=[]
-						b.append(book.title)
-						b.append(book.url)
-						feeds.append(b)
-				if user and user.kindle_email:
-					q.enqueue(pushwork,user.kindle_email,feeds)
 
+tz = pytz.timezone('GMT')
+date = datetime.now(tz)
+hour = date.hour
+users =  model.get_current_push_users(hour)
+if len(users) == 0:
+	print '-=end=-'
+else:
+	#推送
+	for user in users:
+		feeds = []
+		ownfeeds = model.username2feeds(user.name)
+		if len(ownfeeds) != 0:
+			books = (model.get_allbooks())
+			for book in books:
+				if book.f_id in ownfeeds:
+					b=[]
+					b.append(book.title)
+					b.append(book.url)
+					feeds.append(b)
+			if user and user.kindle_email:
+				q.enqueue(pushwork,user.kindle_email,feeds)
 		print '-=end=-'
-
