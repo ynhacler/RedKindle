aad4f681af1454b8bf82e4875412941ff7c9ed8b
zhangzhenhong <zhangzhenhong@zzh.local>
2015-06-23 11:09:23 +0800

    test

--- worker.py
+++ worker.py
@@ -11,6 +11,7 @@ redis_url = os.getenv('REDISTOGO_URL','redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
+
 if __name__ == '__main__':
 	with Connection(conn):
 		worker = Worker(map(Queue,listen))
